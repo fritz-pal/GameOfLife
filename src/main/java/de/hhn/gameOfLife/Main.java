63659c4356dbc4eb21edcf3528975b39deaaf735
fritz-pal <henri@staudenrausch.de>
2023-06-15 12:15:42 +0200

    removed desktop image because of lag

--- src/main/java/de/hhn/gameOfLife/Main.java
+++ src/main/java/de/hhn/gameOfLife/Main.java
@@ -22,16 +22,4 @@ public class Main {
         ImageIcon icon = new ImageIcon("src/main/resources/icons/" + path);
         return new ImageIcon(icon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH));
     }
-
-    public static Image getBackground() {
-        String appData = System.getenv("APPDATA");
-        String wallpaperPath = appData + "\\Microsoft\\Windows\\Themes\\TranscodedWallpaper";
-        File wallpaperFile = new File(wallpaperPath);
-        try {
-            return ImageIO.read(wallpaperFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
